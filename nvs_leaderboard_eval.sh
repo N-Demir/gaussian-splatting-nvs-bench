d16068f885bbc1fc6964477dbf68bbbd88bf87ac
Nikita Demir <nikitde1@gmail.com>
2025-08-05 13:36:15 -0400

    small touch up

--- nvs_leaderboard_eval.sh
+++ nvs_leaderboard_eval.sh
@@ -10,7 +10,8 @@ fi
 scene=$1
 
 method="gaussian-splatting"
-iterations=10
+iterations=30000
+expected_output_folder="/nvs-leaderboard-output/$scene/$method/renders_test"
 
 # Remove the output folder if it already exists
 rm -rf /nvs-leaderboard-output/$scene/$method
@@ -18,13 +19,17 @@ rm -rf /nvs-leaderboard-output/$scene/$method
 # Record start time
 start_time=$(date +%s)
 
+######## START OF YOUR CODE ########
+
 # Train -- note that we do not pass in "--eval" so that the full train split is used
 python train.py -s /nvs-leaderboard-data/$scene/train -m /nvs-leaderboard-output/$scene/$method/ --iterations $iterations
 
 # Render the test split -- note how technically gaussian-splatting places them in the "train" output folder despite them coming from the test split folder
 python render.py -s /nvs-leaderboard-data/$scene/test -m /nvs-leaderboard-output/$scene/$method/ 
 # Move the renders to the expected nvs-leaderboard output folder
-mv /nvs-leaderboard-output/$scene/$method/train/ours_$iterations/renders /nvs-leaderboard-output/$scene/$method/renders_test
+mv /nvs-leaderboard-output/$scene/$method/train/ours_$iterations/renders $expected_output_folder
+
+######## END OF YOUR CODE ########
 
 # Record end time and show duration
 end_time=$(date +%s)
